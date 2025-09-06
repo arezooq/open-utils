b9cae0dc20fc488d2e3f0eb8b6f00e6e062fa7d7
arezooq <ghorbanzadeharezoo@gmail.com>
2025-09-06 11:59:09 +0330

    fixed errors all methods in base repository

--- db/repository/base_repo.go
+++ db/repository/base_repo.go
@@ -1,6 +1,7 @@
 package repository
 
 import (
+	"github.com/arezooq/open-utils/errors"
 	"gorm.io/gorm"
 )
 
@@ -12,7 +13,7 @@ type BasePostgresRepository[T any] struct {
 func (r *BasePostgresRepository[T]) Create(entity *T) (*T, error) {
 	result := r.DB.Create(entity)
 	if result.Error != nil {
-		return nil, result.Error
+		return nil, errors.ErrInternal
 	}
 	return entity, nil
 }
@@ -22,7 +23,10 @@ func (r *BasePostgresRepository[T]) GetById(id uint) (*T, error) {
 	var entity T
 	result := r.DB.First(&entity, id)
 	if result.Error != nil {
-		return nil, result.Error
+		if result.Error == gorm.ErrRecordNotFound {
+			return nil, errors.ErrNotFound
+		}
+		return nil, errors.ErrInternal
 	}
 	return &entity, nil
 }
@@ -30,9 +34,9 @@ func (r *BasePostgresRepository[T]) GetById(id uint) (*T, error) {
 // Get all
 func (r *BasePostgresRepository[T]) GetAll() ([]T, error) {
 	var entities []T
-	result := r.DB.Find(entities)
+	result := r.DB.Find(&entities)
 	if result.Error != nil {
-		return nil, result.Error
+		return nil, errors.ErrInternal
 	}
 	return entities, nil
 }
@@ -42,20 +46,30 @@ func (r *BasePostgresRepository[T]) Update(id uint, updates map[string]any) (*T,
 	var entity T
 	result := r.DB.Model(&entity).Where("id = ?", id).Updates(updates)
 	if result.Error != nil {
-		return nil, result.Error
+		return nil, errors.ErrInternal
 	}
 
 	if err := r.DB.First(&entity, id).Error; err != nil {
-        return nil, err
-    }
+		return nil, errors.ErrNotFound
+	}
 	return &entity, nil
 }
 
 // Delete
-func (r *BasePostgresRepository[T]) Delete(entity *T) error {
-	result := r.DB.Delete(entity)
+func (r *BasePostgresRepository[T]) Delete(id uint) error {
+	var entity T
+	result := r.DB.Delete(&entity, id)
+	
 	if result.Error != nil {
-		return result.Error
+		if result.Error == gorm.ErrRecordNotFound {
+			return errors.ErrNotFound
+		}
+		return errors.ErrInternal
 	}
+
+	if result.RowsAffected == 0 {
+		return errors.ErrNotFound
+	}
+
 	return nil
 }
