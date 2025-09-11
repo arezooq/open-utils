98e3a9a63f91d4a564a9b353c57229c63a6f8308
arezooq <ghorbanzadeharezoo@gmail.com>
2025-09-11 18:07:32 +0330

    added generate refresh token  to jwt open utils

--- jwt/jwt.go
+++ jwt/jwt.go
@@ -34,3 +34,28 @@ func GenerateAccessToken(userID uuid.UUID) (string, error) {
 
 	return tokenString, nil
 }
+
+func GenerateRefreshToken(userID uuid.UUID) (string, error) {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		return "", errors.ErrInternal
+	}
+
+	JWTSecret := os.Getenv("SECRET_JWT")
+
+	claims := jwt.RegisteredClaims{
+		Subject:   userID.String(),
+		IssuedAt:  jwt.NewNumericDate(time.Now().UTC()),
+		ExpiresAt: jwt.NewNumericDate(time.Now().Add(7 * 24 * time.Hour).UTC()), // 7 days
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	tokenString, err := token.SignedString([]byte(JWTSecret))
+	if err != nil {
+		return "", errors.ErrInternal
+	}
+
+	return tokenString, nil
+}
