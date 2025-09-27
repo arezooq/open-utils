7787c3a17ba4208ca99204dab963263377b745df
arezooq <ghorbanzadeharezoo@gmail.com>
2025-09-27 18:07:33 +0330

    edit base redis reop

--- db/repository/base_redis_repo.go
+++ db/repository/base_redis_repo.go
@@ -21,7 +21,7 @@ func NewBaseRedisRepository(client *redis.Client, ctx context.Context) *BaseRedi
 }
 func (r *BaseRedisRepository) Set(key string, value interface{}, ttl time.Duration) error {
 	if err := r.client.Set(r.ctx, key, value, ttl).Err(); err != nil {
-		return errors.ErrRedis
+		return errors.ErrRedisOperation
 	}
 	return nil
 }
@@ -32,14 +32,14 @@ func (r *BaseRedisRepository) Get(key string) (string, error) {
 		return "", errors.ErrNotFound
 	}
 	if err != nil {
-		return "", errors.ErrRedis
+		return "", errors.ErrRedisOperation
 	}
 	return val, nil
 }
 
 func (r *BaseRedisRepository) Delete(key string) error {
 	if err := r.client.Del(r.ctx, key).Err(); err != nil {
-		return errors.ErrRedis
+		return errors.ErrRedisOperation
 	}
 	return nil
 }
@@ -47,14 +47,14 @@ func (r *BaseRedisRepository) Delete(key string) error {
 func (r *BaseRedisRepository) Exists(key string) (bool, error) {
 	n, err := r.client.Exists(r.ctx, key).Result()
 	if err != nil {
-		return false, errors.ErrRedis
+		return false, errors.ErrRedisOperation
 	}
 	return n > 0, nil
 }
 
 func (r *BaseRedisRepository) Expire(key string, ttl time.Duration) error {
 	if err := r.client.Expire(r.ctx, key, ttl).Err(); err != nil {
-		return errors.ErrRedis
+		return errors.ErrRedisOperation
 	}
 	return nil
 }
