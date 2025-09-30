3a8c79d2cbbf774c33522f09cce9f103e0c556db
arezooq <ghorbanzadeharezoo@gmail.com>
2025-09-30 12:36:55 +0330

    edit logger

--- logger/logger.go
+++ logger/logger.go
@@ -8,36 +8,45 @@ import (
 )
 
 type Logger struct {
-	serviceName string
-	logger      *log.Logger
+    serviceName string
+    requestID   string
+    logger      *log.Logger
 }
 
 func New(serviceName string) *Logger {
-	return &Logger{
-		serviceName: serviceName,
-		logger:      log.New(os.Stdout, "", 0),
-	}
+    return &Logger{
+        serviceName: serviceName,
+        logger:      log.New(os.Stdout, "", 0),
+    }
 }
 
-func (l *Logger) log(level LogLevel, requestID, message string) {
-	timestamp := time.Now().Format(time.RFC3339)
-	entry := fmt.Sprintf("[%s] [%s] [%s] [%s] %s",
-		timestamp, l.serviceName, requestID, level, message)
-
-	switch level {
-	case ERROR, WARN, INFO, DEBUG:
-		l.logger.Println(entry)
-	case FATAL:
-		l.logger.Println(entry)
-		os.Exit(1)
-	case PANIC:
-		l.logger.Panicln(entry)
-	}
+func (l *Logger) WithRequestID(reqID string) *Logger {
+    return &Logger{
+        serviceName: l.serviceName,
+        requestID:   reqID,
+        logger:      l.logger,
+    }
 }
 
-func (l *Logger) Info(reqID, msg string)  { l.log(INFO, reqID, msg) }
-func (l *Logger) Warn(reqID, msg string)  { l.log(WARN, reqID, msg) }
-func (l *Logger) Error(reqID, msg string) { l.log(ERROR, reqID, msg) }
-func (l *Logger) Debug(reqID, msg string) { l.log(DEBUG, reqID, msg) }
-func (l *Logger) Fatal(reqID, msg string) { l.log(FATAL, reqID, msg) }
-func (l *Logger) Panic(reqID, msg string) { l.log(PANIC, reqID, msg) }
+func (l *Logger) log(level LogLevel, message string) {
+    timestamp := time.Now().Format(time.RFC3339)
+    entry := fmt.Sprintf("[%s] [%s] [%s] [%s] %s",
+        timestamp, l.serviceName, l.requestID, level, message)
+
+    switch level {
+    case ERROR, WARN, INFO, DEBUG:
+        l.logger.Println(entry)
+    case FATAL:
+        l.logger.Println(entry)
+        os.Exit(1)
+    case PANIC:
+        l.logger.Panicln(entry)
+    }
+}
+
+func (l *Logger) Info(msg string)  { l.log(INFO, msg) }
+func (l *Logger) Warn(msg string)  { l.log(WARN, msg) }
+func (l *Logger) Error(msg string) { l.log(ERROR, msg) }
+func (l *Logger) Debug(msg string) { l.log(DEBUG, msg) }
+func (l *Logger) Fatal(msg string) { l.log(FATAL, msg) }
+func (l *Logger) Panic(msg string) { l.log(PANIC, msg) }
