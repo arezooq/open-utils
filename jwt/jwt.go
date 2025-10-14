95ae8ce4b18d9856627826b6397ffa9db2fcb566
arezooq <ghorbanzadeharezoo@gmail.com>
2025-10-15 01:18:39 +0330

    Edit jwt

--- jwt/jwt.go
+++ jwt/jwt.go
@@ -5,75 +5,46 @@ import (
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
-	"github.com/google/uuid"
-	"github.com/joho/godotenv"
-	"github.com/arezooq/open-utils/errors"
 )
 
 type CustomClaims struct {
-    UserID uuid.UUID
+    UserID string `json:"user_id"`
+    jwt.RegisteredClaims
 }
 
-func GenerateAccessToken(userID uuid.UUID) (string, error) {
-	err := godotenv.Load(".env")
-
-	if err != nil {
-		return "", errors.ErrInternal
-	}
-
-	JWTSecret := os.Getenv("SECRET_JWT")
-
-	claims := jwt.RegisteredClaims{
-		Subject:   userID.String(),
-		IssuedAt:  jwt.NewNumericDate(time.Now().UTC()),
-		ExpiresAt: jwt.NewNumericDate(time.Now().Add(1 * time.Hour).UTC()),
-	}
-
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-
-	tokenString, err := token.SignedString([]byte(JWTSecret))
-	if err != nil {
-		return "", errors.ErrInternal
-	}
-
-	return tokenString, nil
+func GenerateAccessToken(userID string) (string, error) {
+    secret := os.Getenv("SECRET_JWT")
+    claims := CustomClaims{
+        UserID: userID,
+        RegisteredClaims: jwt.RegisteredClaims{
+            IssuedAt:  jwt.NewNumericDate(time.Now().UTC()),
+            ExpiresAt: jwt.NewNumericDate(time.Now().Add(1 * time.Hour)),
+        },
+    }
+    token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+    return token.SignedString([]byte(secret))
 }
 
-func GenerateRefreshToken(userID uuid.UUID) (string, error) {
-	err := godotenv.Load(".env")
-
-	if err != nil {
-		return "", errors.ErrInternal
-	}
-
-	JWTSecret := os.Getenv("SECRET_JWT")
-
-	claims := jwt.RegisteredClaims{
-		Subject:   userID.String(),
-		IssuedAt:  jwt.NewNumericDate(time.Now().UTC()),
-		ExpiresAt: jwt.NewNumericDate(time.Now().Add(7 * 24 * time.Hour).UTC()), // 7 days
-	}
-
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-
-	tokenString, err := token.SignedString([]byte(JWTSecret))
-	if err != nil {
-		return "", errors.ErrInternal
-	}
-
-	return tokenString, nil
+func GenerateRefreshToken(userID string) (string, error) {
+    secret := os.Getenv("REFRESH_SECRET")
+    claims := CustomClaims{
+        UserID: userID,
+        RegisteredClaims: jwt.RegisteredClaims{
+            IssuedAt:  jwt.NewNumericDate(time.Now().UTC()),
+            ExpiresAt: jwt.NewNumericDate(time.Now().Add(7 * 24 * time.Hour)),
+        },
+    }
+    token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+    return token.SignedString([]byte(secret))
 }
 
 func ValidateRefreshToken(tokenStr string) (*CustomClaims, error) {
-    token, err := jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
+    claims := &CustomClaims{}
+    _, err := jwt.ParseWithClaims(tokenStr, claims, func(token *jwt.Token) (interface{}, error) {
         return []byte(os.Getenv("REFRESH_SECRET")), nil
     })
-    if err != nil || !token.Valid {
+    if err != nil {
         return nil, err
     }
-
-    claims := token.Claims.(jwt.MapClaims)
-    return &CustomClaims{
-        UserID: uuid.MustParse(claims["user_id"].(string)),
-    }, nil
+    return claims, nil
 }
